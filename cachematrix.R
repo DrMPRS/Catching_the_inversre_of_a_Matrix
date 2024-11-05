a255298a25831c8c811a5f94efffbf45a1992bcd
DrMPRS <markosprogios@yahoo.com>
2024-11-05 13:20:16 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { ##Creation of the function "makeCacheMatrix", that creates a special matrix that can cache its inverse.
 
 }
 
@@ -12,4 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+} ##in "cacheSolve" if the inverse has already been calculated, cachesolve can retrieve the inverse from the cache, otherwise if the inverse has not been calculated, data gets the matrix stored with makeCacheMatrix, m calculates the inverse, and x$setmean(z) stores it in the object z in makeCacheMatrix.
